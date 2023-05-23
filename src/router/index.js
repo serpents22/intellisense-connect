50983bfa45c79533452c5ee3d6088243e0b5c3af
serpents22 <ivandarmawan67@gmail.com>
2023-05-23 18:41:25 +0700

    feat: change routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import GatewayConfig from '@/views/Devices/GatewayConfig.vue'
-import GatewayDataView from '@/views/Data/GatewayDataView.vue'
 import LoginForm from '@/views/Auth/LoginFormNew.vue'
 import MainMap from '@/views/Map/MainMap.vue'
 import DevicesCategory from '@/views/Devices/DevicesCategory.vue'
-import BLEList from '@/views/Devices/DevicesList.vue'
-import GateList from '@/views/Devices/Gate/GateList.vue'
-import GateDetail from '@/views/Devices/Gate/GateDetail.vue'
+import DevicesList from '@/views/Devices/DevicesLists.vue'
 import DeviceDetails from '@/views/Devices/DeviceDetails.vue'
 import ResetPassword from '@/views/Auth/ResetPassword.vue'
 import NotFound from '@/views/error/NotFound.vue'
+import Setting from '@/views/Setting/Setting.vue'
+
 
 
 const routes = [
   { path: '/', name: 'LoginForm', component: LoginForm, meta: { requiresAuth:false, title: 'Intellisense Connect - Login' } },
   { path: '/reset-password', name: 'ResetPassword', component: ResetPassword, meta: { requiresAuth:false} },
-  { path: '/realtime-map', name: 'MainMap', component: MainMap, meta: { requiresAuth:false } },
-  { path: '/devices', name: 'DevicesCategory', component: DevicesCategory, meta: { requiresAuth:false } },
-  { path: '/devices/ble', name: 'BLEList', component: BLEList, meta: { requiresAuth:false } },
-  { path: '/devices/gate', name: 'GateList', component: GateList, meta: { requiresAuth:false } },
-  { path: '/devices/gate/details/:id', name: 'GateDetail', component: GateDetail, props: true, meta: { requiresAuth:false } },
-  { path: '/devices/device-info/:id', name: 'DeviceDetails', component: DeviceDetails, props: true, meta: { requiresAuth:false } },
-
-  { path: '/data/gateway-data', name: 'GatewayDataView', component: GatewayDataView, meta: { requiresAuth:false } },
+  { path: '/realtime-map', name: 'MainMap', component: MainMap, meta: { requiresAuth:true } },
+  { path: '/devices', name: 'DevicesCategory', component: DevicesCategory, meta: { requiresAuth:true } },
+  { path: '/devices/devices-list/:id', name: 'DevicesList', component: DevicesList, props: true, meta: { requiresAuth:true } },
+  { path: '/devices/device-info/:id', name: 'DeviceDetails', component: DeviceDetails, props: true, meta: { requiresAuth:true } },
+  { path: '/setting', name: 'Setting', component: Setting, meta: { requiresAuth:true } },
 
 ]
 
